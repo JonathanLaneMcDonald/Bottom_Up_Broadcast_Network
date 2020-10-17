5c5d5c034fd39e5dbf10fb86ef3d12e316917890
Lane McDonald <Jonathan.Lane.McDonald@gmail.com>
2020-10-17 16:23:24 +1100

    upload lists, reformatted scripts, and a trained model

--- src/resampler.py
+++ src/resampler.py
@@ -1,16 +1,16 @@
+
+"""
+Resample audio
+"""
+
 import librosa
 import numpy as np
 
-audio_files = [x for x in open('audiofiles.txt','r').read().split('\n') if len(x)]
-print (len(audio_files),'audio files found')
+audio_files = [x for x in open('audiofiles.txt', 'r').read().split('\n') if len(x)]
+print(len(audio_files), 'audio files found')
 
 sample_rate = 8192
 for af in audio_files:
-	data, sr = librosa.load(af,sr=sample_rate)
-	open(af,'wb').write(np.array(data).tostring())
-	print ('wrote',af,'at',sr)
-	
-
-
-
-
+	data, sr = librosa.load(af, sr=sample_rate)
+	open(af, 'wb').write(np.array(data).tostring())
+	print('wrote', af, 'at', sr)
